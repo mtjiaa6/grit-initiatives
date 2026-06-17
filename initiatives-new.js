002b4c353c9ad25a830546a76121ac9ef65230a0
mtjiaa6 <miguel.int@propertyguru.com.sg>
2026-06-17 11:41:11 +0800

    Update initiatives

--- initiatives-new.js
+++ initiatives-new.js
@@ -1,4 +1,4 @@
 // New initiatives added via form submissions
 const NEW_DATA = {
-  // @@NEW_ENTRIES_HERE@@
+  "proj-2026-06-17T03:41:10.9801704Z": { "id": "proj-2026-06-17T03:41:10.9801738Z", "title": "test", "sub": "test", "lens": "extend", "status": "live", "markets": ["MY"], "owner": "test", "team": "test", "started": "test", "launched": "test", "lastUpdated": "2026-06-17T03:41:10.9802088Z", "challenge": "test", "approach": "test", "next": "test", "responder": "miguel.int@propertyguru.com.sg", "kpis": [] },  // @@NEW_ENTRIES_HERE@@
 };
